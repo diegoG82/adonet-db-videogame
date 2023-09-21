d62419b7bfe5ce66f17685617cbb85e8670bf438
diegoG82 <diegogiardina@hotmail.it>
2023-09-21 13:46:33 +0200

    Aggiungere i file di progetto.

---
+++ adonet-db-videogame/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Data Source = localhost; Initial Catalog = vg_database; Integrated Security = True; Pooling = False
---
+++ adonet-db-videogame/Videogame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adonet_db_videogame
+{
+    public class Videogame
+    {
+        public long Id { get; private set; }
+        public string Name { get; private set; }
+        public string Overview { get; private set; }
+        public DateTime ReleaseDate { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+        public long SoftwareHouseId { get; private set; }
+
+        public Videogame(long id, string name, string overview, DateTime releaseDate, DateTime createdAt, DateTime updatedAt, long softwareHouseId)
+        {
+            Id = id;
+            Name = name;
+            Overview = overview;
+            ReleaseDate = releaseDate;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+            SoftwareHouseId = softwareHouseId;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Overview: {Overview}, Release Date: {ReleaseDate}, Created At: {CreatedAt}, Updated At: {UpdatedAt}, Software House ID: {SoftwareHouseId}";
+        }
+    }
+
+}
