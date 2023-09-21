d736e5af4c4a25b320dc70c084a2d8e13039723e
diegoG82 <diegogiardina@hotmail.it>
2023-09-21 16:32:12 +0200

    Fatte prime 2 funzioni

--- adonet-db-videogame/Program.cs
+++ adonet-db-videogame/Program.cs
@@ -1,4 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using adonet_db_videogame;
 
-Data Source = localhost; Initial Catalog = vg_database; Integrated Security = True; Pooling = False
+Console.WriteLine("Welcome to our Videogames manager!!!");
+
+
+while (true)
+{
+    Console.WriteLine(@"
+- 1: Create VideoGames 
+- 2: Show VideoGames by id
+- 3: Modify VideoGames
+- 4: Delete VideoGames
+- 5: Exit 
+
+");
+
+    Console.Write("Make your choice: ");
+
+    int selectedOption = int.Parse(Console.ReadLine());
+
+    switch (selectedOption)
+    {
+        //CREATE A VIDEOGAME
+        case 1:
+            {
+                Console.WriteLine("Create a Videogame:");
+                Console.WriteLine("Enter the title of the game: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter the overview of the game: ");
+                string overview = Console.ReadLine();
+                Console.WriteLine("Enter the release date of the game: ");
+                DateTime releasedate = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the software house ID of the game: ");
+                long softwareHouseId = long.Parse(Console.ReadLine());
+
+                Videogame vg = new Videogame(0, name, overview, releasedate, softwareHouseId);
+                bool inserted = VideogameManager.AddVideogame(vg);
+
+                if (inserted)
+                {
+                    Console.WriteLine("Game added!");
+                }
+                else
+                {
+                    Console.WriteLine("Game not created!");
+                }
+
+
+
+
+
+            }
+            break;
+
+        //FILTER BY ID
+        case 2:
+
+            Console.WriteLine("Insert Game Id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Videogame videogame = VideogameManager.GetVideogameById(id);
+
+            if (videogame != null)
+            {
+                Console.WriteLine($"Videogame found: {videogame.Name}");
+            }
+            else
+            {
+                Console.WriteLine("No videogame found with the specified ID.");
+            }
+
+            break;
+
+        case 5:
+            Console.WriteLine("Exiting the program...");
+            return;
+
+
+        default:
+            Console.WriteLine("No options choosed");
+            break;
+
+    }
+
+
+}
---
+++ adonet-db-videogame/VideogameManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Data.SqlClient;
+
+namespace adonet_db_videogame
+{
+    public static class VideogameManager
+    {
+        private static string connectionString = "Data Source = localhost; Initial Catalog = vg_database; Integrated Security = True; Pooling = False";
+
+        public static bool AddVideogame(Videogame videogame)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO videogames (name, overview, release_date, software_house_id) VALUES (@name, @overview, @releaseDate, @softwareHouseId);";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", videogame.Name);
+                        command.Parameters.AddWithValue("@overview", videogame.Overview);
+                        command.Parameters.AddWithValue("@releaseDate", videogame.ReleaseDate);
+                        command.Parameters.AddWithValue("@softwareHouseId", videogame.SoftwareHouseId);
+
+                        int row = command.ExecuteNonQuery();
+                        if (row > 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                return false;
+            }
+        }
+
+
+
+
+
+
+
+        //READ
+        public static Videogame GetVideogameById(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = "SELECT id, name, overview, release_date, software_house_id FROM videogames WHERE id = @id;";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                long Id = reader.GetInt64(0);
+                                string name = reader.GetString(1);
+                                string overview = reader.GetString(2);
+                                DateTime releaseDate = reader.GetDateTime(3);
+                            
+                                long softwareHouseId = reader.GetInt64(4);
+
+                                Videogame videogame = new Videogame(Id, name, overview, releaseDate, softwareHouseId);
+
+                                return videogame;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
